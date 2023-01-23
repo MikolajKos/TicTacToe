5fb194c088dbd71cb53b101f3333e64850d4eca4
Mikołaj <95126739+MikolajKos@users.noreply.github.com>
2023-01-23 11:39:40 +0100

    Add files via upload

---
+++ TicTacToeApp/TicTacToeApp/MVVM/Models/MainModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeApp.MVVM.Models
+{
+    public class MainModel
+    {
+        public bool isCrossTurn = false;
+        public bool isCircleTurn = true;
+    }
+}
---
+++ TicTacToeApp/TicTacToeApp/MVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using UnitsConverterApp.Core;
+
+namespace TicTacToeApp.MVVM.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private int _contentValue;
+        public int ContentValue
+        {
+            get => _contentValue; 
+            set
+            {
+                _contentValue = value;
+                OnPropertyChanged(nameof(ContentValue));
+            }
+        }
+
+
+
+        #region Commands
+
+        private ICommand _playerPutsASign;
+        public ICommand PlayerPutsASign
+        {
+            get
+            {
+                if (_playerPutsASign == null) _playerPutsASign = new RelayCommand(
+                    (object o) =>
+                    {
+                        var clickedButton = o.ToString();
+                        Button bt = new Button();
+                        bt.Content = "ASD";
+                    },
+                    (object o) => true);
+                return _playerPutsASign;
+            }
+        }
+
+        #endregion
+    }
+}
